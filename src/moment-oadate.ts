cd9370adde31b8c20d9761ac0b4f2ae9a0a433db
Anant Singh <anant@matelabs.in>
2021-07-04 14:28:31 +0530

    Implemented both methods

--- src/moment-oadate.ts
+++ src/moment-oadate.ts
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-// Extend types
+// Extend Moment types
 declare module "moment" {
   interface Moment {
     /**
@@ -8,12 +8,29 @@ declare module "moment" {
      * @returns number
      */
     toOADate(): number;
-    fromOADate(oaDate: number): moment.Moment;
   }
 }
 
-const DAY_MILLISECONDS = 86400000;
-const MS_DAY_OFFSET = 25569;
+const DAY_TO_MILLISECONDS = 86400000;
+const OADATE_DAY_OFFSET = 25569;
+
+/**
+ * Returns the moment object for the provided OADate value
+ * @param { number } oaDate value
+ * @returns { moment.Moment } moment date
+ */
+export const fromOADate = function (oaDate: number): moment.Moment {
+  let ms = (oaDate - OADATE_DAY_OFFSET) * DAY_TO_MILLISECONDS;
+
+  if (oaDate < 0) {
+    const frac = (oaDate - Math.trunc(oaDate)) * DAY_TO_MILLISECONDS;
+    if (frac !== 0) {
+      ms -= frac * 2;
+    }
+  }
+
+  return moment(ms).utc();
+};
 
 function extendMoment(): void {
   /**
@@ -24,14 +41,17 @@ function extendMoment(): void {
     // Get moment time in milliseconds
     const ms = this.valueOf();
 
-    // TODO: Handle negative values
-    return ms / DAY_MILLISECONDS + MS_DAY_OFFSET;
-  };
+    let oaDate = ms / DAY_TO_MILLISECONDS + OADATE_DAY_OFFSET;
+    if (oaDate < 0) {
+      const fractionPart = oaDate - Math.trunc(oaDate);
+      if (fractionPart !== 0) {
+        oaDate = Math.ceil(oaDate) - fractionPart - 2;
+      }
+    }
 
-  moment.prototype.fromOADate = function (oaDate: number): moment.Moment {
-    // TODO: Implement method
-    return this;
+    return oaDate;
   };
 }
 
+// Run the method to extend moment prototype
 extendMoment();
--- src/test/test.ts
+++ src/test/test.ts
@@ -1,12 +1,17 @@
 import moment from "moment";
 import "../moment-oadate";
+import { fromOADate } from "../moment-oadate";
 
 const m = moment.utc("21/10/2015", "DD/MM/YYYY");
 
 // Moment's native method
-let date = m.toDate();
-console.log("UTC:", date);  // 2015-10-21T00:00:00.000Z
+const date = m.toDate();
+console.log("Input UTC date:", date);  // 2015-10-21T00:00:00.000Z
 
-// Extended method
+// Extended toOADate method
 const oaDate = m.toOADate();
-console.log("OADate:", oaDate); // 42298
+console.log("Moment to OADate:", oaDate); // 42298
+
+// Moment from OADate method
+const m2 = fromOADate(42298)
+console.log("Moment from OADate:", m2.toDate());  // 2015-10-21T00:00:00.000Z
