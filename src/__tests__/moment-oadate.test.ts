e910416c7073c91e4fdbdd07c4081f05f07490e9
Anant Singh <anant@matelabs.in>
2021-07-11 20:11:21 +0530

    Added tests

---
+++ src/__tests__/moment-oadate.test.ts
@@ -0,0 +1,28 @@
+import * as moment from "moment";
+import "../moment-oadate";
+import { fromOADate } from "../moment-oadate";
+
+/**
+ * Date used for testing this package
+ */
+const MOMENT_DATE_1 = moment.utc("21/10/2015", "DD/MM/YYYY");
+/**
+ * Equivalent OA date
+ */
+const OA_DATE_1 = 42298
+
+describe("fromOADate", () => {
+  test("fromOADate to Moment object", () => {
+    const momentDate = fromOADate(OA_DATE_1);
+
+    expect(momentDate.toDate()).toMatchObject(MOMENT_DATE_1.toDate());
+  });
+});
+
+describe("toOADate", () => {
+  test("Moment object to toOADate", () => {
+    const oaDate = MOMENT_DATE_1.toOADate();
+
+    expect(oaDate).toBe(OA_DATE_1);
+  });
+});
